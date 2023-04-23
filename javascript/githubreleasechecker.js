851c48c3712c8bc97c74833342d2cd04eb0b17e5
Toby-420 <83547635+Toby-420@users.noreply.github.com>
2023-04-23 20:06:44 +0100

    Update githubreleasechecker.js

--- javascript/githubreleasechecker.js
+++ javascript/githubreleasechecker.js
@@ -1,15 +1,27 @@
 const token = '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';
-const url = `https://api.github.com/repos/Toby-420/text-adventure/releases/latest`;
-
-fetch(url, {
-    headers: {
-        Authorization: `token ${token}`
-    }
-})
-.then(response => response.json())
-.then(data => {
-    const releaseDescription = data.body;
-    // Use the releaseDescription however you like, for example:
-    document.getElementById('release-description').innerText = releaseDescription;
-})
-.catch(error => console.error(error));
+const url = `https: //api.github.com/repos/Toby-420/text-adventure/releases/latest`;
+fetch(apiUrl, {
+  headers: {
+    Authorization: `token $ {
+      accessToken
+    }`,
+  },
+}).then(response = >{
+  if (!response.ok) {
+    throw new Error(`Failed to fetch release data: $ {
+      response.statusText
+    }`);
+  }
+  return response.json();
+}).then(data = >{
+  if (!data.body) {
+    throw new Error('Release notes not found.');
+  }
+  const description = data.body;
+  const releaseDescription = document.getElementById('release-description');
+  releaseDescription.innerText = description;
+}).
+catch(error = >{
+  console.error(error);
+  const releaseDescription = document.getElementById('release-description');
+  releaseDescription.innerText = 'Failed to fetch release notes.';
