789415a14e274026573189e3158c414a374647c7
Toby-420 <83547635+Toby-420@users.noreply.github.com>
2023-04-23 22:52:33 +0100

    QoL for the githubreleasechecker.js
    Add newline for every fullstop, space, and capital letter

--- javascript/githubreleasechecker.js
+++ javascript/githubreleasechecker.js
@@ -1,18 +1,16 @@
-// Specify the URL of the JSON data
 const url = "https://api.github.com/repos/Toby-420/text-adventure/releases/latest";
 
-
-// Use the fetch() method to retrieve the data from the URL
 fetch(url)
-  .then(response => response.json()) // Convert the response to a JavaScript object
+  .then(response => response.json())
   .then(data => {
-    // Retrieve the "body" field of the JSON data
     const bodyData = data.body;
 
-    // Display the "body" field in the "release-description" div on your website
+    // Add newline or <br> after fullstop and space, and before capital letter
+    const formattedBodyData = bodyData.replace(/\. +([A-Z])/g, '.<br>$1');
+
     const releaseDescription = document.getElementById("release-description");
-    releaseDescription.innerHTML = bodyData;
+    releaseDescription.innerHTML = formattedBodyData;
   })
   .catch(error => {
-    console.error(error); // Log any errors to the console
+    console.error(error);
   });
