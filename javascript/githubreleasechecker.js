697afeb5f63828d2d7b54715a73b9823968bc54f
Toby-420 <83547635+Toby-420@users.noreply.github.com>
2023-04-23 19:56:42 +0100

    Update githubreleasechecker.js

--- javascript/githubreleasechecker.js
+++ javascript/githubreleasechecker.js
@@ -1,15 +1,15 @@
-const apiUrl = 'https://api.github.com/repos/Toby-420/text-adventure/releases/latest';
-const accessToken = process.env.GITHUB_TOKEN;
+const token = 'process.env.GITHUB_TOKEN;
+const url = `https://api.github.com/repos/Toby-420/text-adventure/releases/latest`;
 
-fetch(apiUrl, {
-  headers: {
-    Authorization: `token ${accessToken}`,
-  },
+fetch(url, {
+    headers: {
+        Authorization: `token ${token}`
+    }
 })
-  .then(response => response.json())
-  .then(data => {
-    const description = data.body;
-    const releaseDescription = document.getElementById('release-description');
-    releaseDescription.innerText = description;
-  })
-  .catch(error => console.error(error));
+.then(response => response.json())
+.then(data => {
+    const releaseDescription = data.body;
+    // Use the releaseDescription however you like, for example:
+    document.getElementById('release-description').innerText = releaseDescription;
+})
+.catch(error => console.error(error));
