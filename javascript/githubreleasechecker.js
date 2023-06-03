bc4fbdc63d48524f2e2ecc082bed626300d297bf
Toby-420 <83547635+Toby-420@users.noreply.github.com>
2023-06-03 18:10:59 +0100

    Remove wiki

--- javascript/githubreleasechecker.js
+++ javascript/githubreleasechecker.js
@@ -1,16 +1,1 @@
-const url = "https://api.github.com/repos/Toby-420/text-adventure/releases/latest";
-
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    const bodyData = data.body;
-
-    // Add newline or <br> after fullstop and space, and before capital letter
-    const formattedBodyData = bodyData.replace(/\. +([A-Z])/g, '.<br>$1');
-
-    const releaseDescription = document.getElementById("release-description");
-    releaseDescription.innerHTML = formattedBodyData;
-  })
-  .catch(error => {
-    console.error(error);
-  });
+const url="https://api.github.com/repos/Toby-420/text-adventure/releases/latest";fetch(url).then(response=>response.json()).then(data=>{const bodyData=data.body;const formattedBodyData=bodyData.replace(/\. +([A-Z])/g,'.<br>$1');const releaseDescription=document.getElementById("release-description");releaseDescription.innerHTML=formattedBodyData}).catch(error=>{console.error(error)});
